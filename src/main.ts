41d41a63a8c34cbecd5bfd8c3096e322ef8f406b
gustavodiniz <gustavodinniz.dev@gmail.com>
2023-11-15 19:56:38 -0300

    feat: development environment configuration

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import {
 } from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   await app.listen(3000, '0.0.0.0');
 }
+
 bootstrap();
